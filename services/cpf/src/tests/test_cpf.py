0d62135fc6b1c23c96d53f45a10ae78e6d353eb9
glauber silva <glauber.lucio.silva@gmail.com>
2019-03-03 01:32:56 -0300

    - add more tests

--- services/cpf/src/tests/test_cpf.py
+++ services/cpf/src/tests/test_cpf.py
@@ -6,7 +6,7 @@ from src.tests.base import BaseTestCase
 class TestCpfService(BaseTestCase):
     """Tests for the CPF Service"""
 
-    def test_cpf(self):
+    def test_cpf_ping(self):
         """checks if the route / ping (to check sanity) works correctly."""
         response = self.client.get("/cpf/ping")
         data = json.loads(response.data.decode())
@@ -15,5 +15,39 @@ class TestCpfService(BaseTestCase):
         self.assertIn('success', data['status'])
 
 
+    def test_get_cpf_situation(self):
+        """
+        Get cpf situation
+        """
+        cpf = '40442820135'
+        with self.client:
+            response = self.client.get('/cpf/{cpf}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("regular", data["status"])
+
+
+    def test_cpf_is_digit(self):
+        """
+        CPF must only accept digits
+       """
+        cpf = 'ABCD5678901'
+        with self.client:
+            response = self.client.get('/cpf/{cpf}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('CPF Inválido, deve conter somente números', data['error']['reason'])
+
+
+    def test_cpf_has_11_digits(self):
+        """
+        CPF must have 11 digits
+        """
+        cpf = '5678901'
+        with self.client:
+            response = self.client.get('/cpf/{cpf}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('CPF Inválido, deve conter 11 digitos', data['error']['reason'])
 if __name__ == "__main__":
     unittest.main()
