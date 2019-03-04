51ab9f65e3354623620ce4bd072ad5a625663f5e
glauber silva <glauber.lucio.silva@gmail.com>
2019-03-04 12:54:09 -0300

    - added users blueprint

--- services/users/src/__init__.py
+++ services/users/src/__init__.py
@@ -3,9 +3,3 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/users/ping', methods=['GER'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
