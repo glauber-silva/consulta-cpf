6b4b2627e45eadf532f8d1a17c057abf95543adb
glauber silva <glauber.lucio.silva@gmail.com>
2019-03-04 23:59:08 -0300

    - added coverage commands

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,21 @@
 import unittest
+
+import coverage
 from flask.cli import FlaskGroup
 from src import create_app
 from src.ext.db import db
 from src.api.models import User
+
+COV = coverage.coverage(
+    branch=True,
+    include='src/*',
+    omit=[
+        'src/tests/*',
+        'src/config.py'
+    ]
+)
+
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -21,6 +34,20 @@ def seed_db():
     db.session.add(User(username="test", email="test@gmail.com"))
     db.session.commit()
 
+@cli.command()
+def cov():
+    """ Run unit tests with coverage """
+    tests = unittest.TestLoader().discover('src/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary: ")
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 @cli.command()
 def test():
