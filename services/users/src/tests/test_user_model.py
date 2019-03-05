90435b8ee0e44e25c322c2225a9462eb29d49359
glauber silva <glauber.lucio.silva@gmail.com>
2019-03-05 01:28:38 -0300

    - added somes improvements on users tests

--- services/users/src/tests/test_user_model.py
+++ services/users/src/tests/test_user_model.py
@@ -4,46 +4,31 @@ from src.ext.db import db
 from src.api.models import User
 from src.tests.base import BaseTestCase
 from sqlalchemy.exc import IntegrityError
-
+from src.tests.utils import add_user
 
 class TestUserModel(BaseTestCase):
 
     def test_add_user(self):
-        user = User(
-            username='test',
-            email='test@email.com',
-        )
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('test', 'test@email.com')
         self.assertTrue(user.id)
         self.assertEqual(user.username, 'test')
         self.assertEqual(user.email, 'test@email.com')
         self.assertTrue(user.active)
 
     def test_add_user_duplicate_username(self):
-        user = User(
-            username='test',
-            email='test@email.com'
-        )
-        db.session.add(user)
-        db.session.commit()
+        add_user('test', 'test@email.com')
         duplicate_user = User(
             username='test',
-            email='test@email.com'
+            email='test2@email.com',
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_add_user_duplicate_email(self):
-        user = User(
-            username='test',
-            email='test@email.com'
-        )
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('test', 'test@email.com')
         duplicate_user = User(
-            username='othertest',
-            email='test@email.com'
+            username='test2',
+            email='test@email.com',
         )
         db.session.add(duplicate_user)
         self.assertRaises(IntegrityError, db.session.commit)
