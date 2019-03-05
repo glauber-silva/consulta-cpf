23310b59158fb7a5ccc066040f112a9808e7c54a
glauber silva <glauber.lucio.silva@gmail.com>
2019-03-05 00:38:07 -0300

    - added commands for coverage | cpf

--- services/cpf/manage.py
+++ services/cpf/manage.py
@@ -1,7 +1,19 @@
 import unittest
+
+import coverage
 from flask.cli import FlaskGroup
 from src import create_app
 
+COV = coverage.coverage(
+    branch=True,
+    include='src/*',
+    omit=[
+        'src/tests/*',
+        'src/config.py'
+    ]
+)
+
+COV.start()
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
@@ -13,6 +25,22 @@ def test():
     tests = unittest.TestLoader().discover('src/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@cli.command()
+def cov():
+    """ Run unit tests with coverage """
+    tests = unittest.TestLoader().discover('src/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary: ")
+        COV.report()
+        COV.html_report()
+        COV.erase()
         return 0
     return 1
 
